0df69422e49558f07bb2695a830e9a0cf67e0a26
baukobalazs <baukobalazs20@gmail.com>
2026-03-04 20:22:24 +0100

    layout

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,5 +1,6 @@
-import AppLayout from "../../components/layout/AppLayout";
+export const dynamic = 'force-dynamic';
 
+import AppLayout from "../../components/layout/AppLayout";
 
 export default function ProtectedLayout({ children }: { children: React.ReactNode }) {
     return <AppLayout>{children}</AppLayout>;
